7b6fdfa2ace5cbff0dd8a55f785b38d0a4754a5b
wendycheng(成文迪) <wendycheng@tencent.com>
2017-11-24 17:59:35 +0800

    init

---
+++ demo/color-video.js
@@ -0,0 +1,117 @@
+;(function($) {
+  $.fn.colorvideo = function(options) {
+    var defaults = {
+      'videoUrl': '',
+      'videoWidth': 0,
+      'videoHeight': 0,
+      'loop': 1000,
+      'autoplay': true,
+      'muted': false,
+      'validate': true,
+      'onVideoClick': null,
+      'onVideoHover': null,
+    };
+
+    var opts = $.extend({}, defaults, options || {});
+    var canvasMousePos = {
+      x: 0,
+      y: 0
+    };
+    var canvasOffset = {
+      left: 0,
+      top: 0
+    };
+    var colorData = {};
+    var _this = this;
+    init();
+
+    this.play = function() {
+      $(this).find("#myvideo")[0].play()
+    }
+    this.pause = function() {
+      $(this).find("#myvideo")[0].pause()
+    }
+
+    function init() {
+      var colorvideoDom = "<div id='color-video' style='height:" + opts.videoHeight + "px;width:" + opts.videoWidth + "px'> \
+        <canvas id='mycanvas' width=" + opts.videoWidth + " height=" + opts.videoHeight +
+        "></canvas>\
+        <video id='myvideo' crossOrigin='*' src=" + opts.videoUrl + " style='display:none' loop=" + opts.loop + " autoplay=" + opts.autoplay + " muted=" + opts.muted + "></video>\
+        </div>";
+      $(_this).html(colorvideoDom);
+      canvasOffset = offset($(_this).find("#mycanvas")[0])
+      playCanvas();
+      $(_this).on("mousemove", function(e) {
+        var canvasX = Math.floor(e.pageX - canvasOffset.left)
+        var canvasY = Math.floor(e.pageY - canvasOffset.top)
+        canvasMousePos.x = canvasX
+        canvasMousePos.y = canvasY
+        if (opts.onVideoHover != null) {
+          var mycanvas = $(_this).find("#mycanvas")[0]
+          colorData = getPixelColor(mycanvas, canvasMousePos.x, canvasMousePos.y)
+          opts.onVideoHover(e, colorData)
+        }
+      })
+      $(_this).click(function(e) {
+        var mycanvas = $(_this).find("#mycanvas")[0]
+        colorData = getPixelColor(mycanvas, canvasMousePos.x, canvasMousePos.y)
+        opts.onVideoClick(e, colorData)
+      })
+    }
+
+    function playCanvas() {
+      var mycanvas = $(_this).find("#mycanvas")[0]
+      var myvideo = $(_this).find("#myvideo")[0]
+      var context = mycanvas.getContext("2d")
+      context.drawImage(myvideo, 0, 0, opts.videoWidth, opts.videoHeight)
+      colorData = getPixelColor(mycanvas, canvasMousePos.x, canvasMousePos.y)
+      requestAnimationFrame(playCanvas)
+    }
+
+    function getPixelColor(canvas, x, y) {
+      var thisContext = canvas.getContext("2d")
+      var imageData = thisContext.getImageData(x, y, 1, 1)
+      var pixel = imageData.data
+      var r = pixel[0]
+      var g = pixel[1]
+      var b = pixel[2]
+      var a = pixel[3] / 255
+      a = Math.round(a * 100) / 100
+      var rHex = r.toString(16)
+      r < 16 && (rHex = "0" + rHex)
+      var gHex = g.toString(16)
+      g < 16 && (gHex = "0" + gHex)
+      var bHex = b.toString(16)
+      b < 16 && (bHex = "0" + bHex)
+      var rgbaColor = "rgba(" + r + "," + g + "," + b + "," + a + ")"
+      var rgbColor = "rgb(" + r + "," + g + "," + b + ")"
+      var hexColor = "#" + rHex + gHex + bHex
+      return {
+        rgba: rgbaColor,
+        rgb: rgbColor,
+        hex: hexColor,
+        r: r,
+        g: g,
+        b: b,
+        a: a
+      }
+    }
+
+    function offset(obj) {
+      var top = 0,
+        left = 0
+      if (obj) {
+        while (obj.offsetParent) {
+          top += obj.offsetTop
+          left += obj.offsetLeft
+          obj = obj.offsetParent
+        }
+      }
+      return {
+        top: top,
+        left: left
+      }
+    }
+    return this;
+  }
+})(jQuery)
---
+++ src/color-video.vue
@@ -0,0 +1,131 @@
+<style scoped>
+.color-video {
+    position: relative;
+    display: inline-block;
+}
+</style>
+<template>
+<section class="color-video" :style="'height:'+videoHeight+'px;width:'+videoWidth+'px'" @click="clickCanvas">
+    <canvas ref="mycanvas" :width="videoWidth" :height="videoHeight" v-on:mousemove="tranceCanvas"></canvas>
+    <video ref="myvideo" crossOrigin="*" :src="videoUrl" @timeupdate="videoTimeupdate" style="display:none" :loop="loop" :autoplay="autoplay" :muted="muted"></video>
+</section>
+</template>
+<script>
+export default {
+    name: 'video-colorpicker',
+    props: {
+        videoUrl: {
+            type: String,
+            default: ''
+        },
+        videoWidth: {
+            type: Number,
+            default: 0
+        },
+        videoHeight: {
+            type: Number,
+            default: 0
+        },
+        loop: {
+            type: Boolean,
+            default: false
+        },
+        autoplay: {
+            type: Boolean,
+            default: true
+        },
+        muted: {
+            type: Boolean,
+            default: true
+        },
+        validate: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {
+            colorData: {},
+            canvasOffset: {
+                left: 0,
+                top: 0
+            },
+            canvasMousePos: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    created() {
+        var that = this
+        setTimeout(function() {
+            that.playCanvas()
+            var mycanvas = that.$refs.mycanvas
+            that.canvasOffset = that.offset(mycanvas)
+        }, 0)
+    },
+    methods: {
+        playCanvas() {
+            var mycanvas = this.$refs.mycanvas
+            var myvideo = this.$refs.myvideo
+            var context = mycanvas.getContext("2d")
+            context.drawImage(myvideo, 0, 0, this.videoWidth, this.videoHeight)
+            this.colorData = this.getPixelColor(mycanvas, this.canvasMousePos.x, this.canvasMousePos.y)
+            this.$emit('onVideoHover', this.colorData)
+            requestAnimationFrame(this.playCanvas)
+        },
+        tranceCanvas(e) {
+            var canvasX = Math.floor(e.pageX - this.canvasOffset.left)
+            var canvasY = Math.floor(e.pageY - this.canvasOffset.top)
+            this.canvasMousePos.x = canvasX
+            this.canvasMousePos.y = canvasY
+        },
+        clickCanvas(e) {
+            this.$emit('onVideoClick', this.colorData)
+        },
+        offset(obj) {
+            var top = 0,
+                left = 0
+            if (obj) {
+                while (obj.offsetParent) {
+                    top += obj.offsetTop
+                    left += obj.offsetLeft
+                    obj = obj.offsetParent
+                }
+            }
+            return {
+                top: top,
+                left: left
+            }
+        },
+        getPixelColor(canvas, x, y) {
+            var thisContext = canvas.getContext("2d")
+            var imageData = thisContext.getImageData(x, y, 1, 1)
+            var pixel = imageData.data
+            var r = pixel[0]
+            var g = pixel[1]
+            var b = pixel[2]
+            var a = pixel[3] / 255
+            a = Math.round(a * 100) / 100
+            var rHex = r.toString(16)
+            r < 16 && (rHex = "0" + rHex)
+            var gHex = g.toString(16)
+            g < 16 && (gHex = "0" + gHex)
+            var bHex = b.toString(16)
+            b < 16 && (bHex = "0" + bHex)
+            var rgbaColor = "rgba(" + r + "," + g + "," + b + "," + a + ")"
+            var rgbColor = "rgb(" + r + "," + g + "," + b + ")"
+            var hexColor = "#" + rHex + gHex + bHex
+            return {
+                rgba: rgbaColor,
+                rgb: rgbColor,
+                hex: hexColor,
+                r: r,
+                g: g,
+                b: b,
+                a: a
+            }
+        },
+    },
+}
+</script>
